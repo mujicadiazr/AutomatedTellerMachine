76ec5334b8a786842fb5cde47fc7064c930599ee
Randy Mujica Diaz <mujicadiazr@gmail.com>
2017-03-20 19:58:04 -0400

    Final Commit

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -50,5 +50,113 @@ namespace AutomatedTellerMachine.Controllers
             return View();
 
         }
+
+        //GET: Transaction/Withdrawal
+        public ActionResult Withdrawal(int checkingAccountId)
+        {
+            ViewBag.CheckingAccountId = checkingAccountId;
+            return View();
+        }
+
+        //POST: Transaction/Withdrawal
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Withdrawal(Transaction transaction)
+        {
+            var checkingAccount = db.CheckingAccounts.Find(transaction.CheckingAccountId);
+
+            if (checkingAccount.Balance < transaction.Amount)
+            {
+                ModelState.AddModelError("Amount", "You have insufficient funds!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                transaction.Amount = -transaction.Amount;
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                CheckingAccountService service = new CheckingAccountService(db);
+                service.UpdateBalace(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
+        public ActionResult QuickCash(int checkingAccountId, int amount)
+        {
+            var checkingAccount = db.CheckingAccounts.Find(checkingAccountId);
+
+            //Validating correct amount
+            if (amount != 100)
+            {
+                ViewBag.ErrorMessage = "ERROR: Incorrect amount for this transaction!";
+                return View("QuickCash");
+            }
+            //Validating sufficient funds
+            if (checkingAccount.Balance < 100)
+            {
+                ViewBag.ErrorMessage = "ERROR: Insufficient funds for this transaction!";
+                return View("QuickCash");
+            }
+
+            var transaction = new Transaction { Amount = -amount, CheckingAccountId = checkingAccountId };
+            db.Transactions.Add(transaction);
+            db.SaveChanges();
+
+            var service = new CheckingAccountService(db);
+            service.UpdateBalace(checkingAccountId);
+            
+            return RedirectToAction("Index","Home");
+        }
+
+        //GET: Transaction/Transfer
+        public ActionResult Transfer(int checkingAccountId)
+        {
+            return View();
+        }
+
+        //POST: Transaction/Transfer
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Transfer(TranferViewModel transfer)
+        {
+            var sourceCheckingAccount = db.CheckingAccounts.Find(transfer.CheckingAccountId);
+            //Check for available funds
+            if (sourceCheckingAccount.Balance < transfer.Amount)
+            {
+                ModelState.AddModelError("Amount", "Insufficient funds for this transfer");
+            }
+
+            var destinationCheckingAccount =
+                db.CheckingAccounts.Where(c => c.AccountNumber == transfer.DestinationCheckingAccountNumber)
+                    .FirstOrDefault();
+
+            //Check for a valid destination account
+            if (destinationCheckingAccount == null)
+            {
+                ModelState.AddModelError("DestinationCheckingAccountNumber", "Invalid destination checking account number");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(new Transaction {Amount = -transfer.Amount, CheckingAccountId = transfer.CheckingAccountId});
+                db.Transactions.Add(new Transaction
+                {
+                    Amount = transfer.Amount,
+                    CheckingAccountId = destinationCheckingAccount.Id
+                });
+
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalace(transfer.CheckingAccountId);
+                service.UpdateBalace(destinationCheckingAccount.Id);
+
+                return PartialView("_TranferResult", transfer);
+            }
+
+            return PartialView("_TransferForm");
+        }
     }
 }
